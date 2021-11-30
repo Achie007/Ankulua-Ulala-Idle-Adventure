8e9b46b68bf6eadde1925da57f9c48dcd62a5221
Achie007 <77725121+Achie007@users.noreply.github.com>
2021-11-30 08:40:53 +0300

    Create CheckFilesandDirectory.lua

---
+++ CheckFilesandDirectory.lua
@@ -0,0 +1,35 @@
+setImmersiveMode(true)
+
+usePreviousSnap(false)
+
+FolderExists(scriptPath().. "image")
+--------------CAMP
+FolderExists(scriptPath().. "image/Camp")
+FolderExists(scriptPath().. "image/Camp/AdventureGuild")
+FolderExists(scriptPath().. "image/Camp/Arena")
+FolderExists(scriptPath().. "image/Camp/HeroTraining")
+--------------Character
+FolderExists(scriptPath().. "image/Character")
+FolderExists(scriptPath().. "image/Character/Bag")
+FolderExists(scriptPath().. "image/Character/Equip")
+FolderExists(scriptPath().. "image/Character/Skill")
+--------------Fight
+FolderExists(scriptPath().. "image/Fight")
+FolderExists(scriptPath().. "image/Fight/AutoAttack")
+FolderExists(scriptPath().. "image/Fight/HareBurrow")
+FolderExists(scriptPath().. "image/Fight/HareBurrow/Exploration")
+FolderExists(scriptPath().. "image/Fight/HareBurrow/GiftBunny")
+FolderExists(scriptPath().. "image/Fight/QuickBattle")
+--------------Character
+FolderExists(scriptPath().. "image/Market")
+FolderExists(scriptPath().. "image/Market/Cook")
+FolderExists(scriptPath().. "image/Market/Cook/FoodList")
+--------------Character
+FolderExists(scriptPath().. "image/OCR")
+--------------Character
+FolderExists(scriptPath().. "image/Pet")
+FolderExists(scriptPath().. "image/Pet/LuckyRuins")
+FolderExists(scriptPath().. "image/Pet/PetAdventure")
+FolderExists(scriptPath().. "image/Pet/PetCapture")
+FolderExists(scriptPath().. "image/Pet/PetRelease")
+
